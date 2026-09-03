19da7e99ddca0046fdab7dae6a496f9131ee7fe4
Keeto1 <znidimourad26@gmail.com>
2026-09-03 22:39:20 +0100

    fixed the photos in abous us section for the members ( more zoomed)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/navbar";
-import Footer from "./components/footer";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { baseMetadata } from "./lib/seo";
 
